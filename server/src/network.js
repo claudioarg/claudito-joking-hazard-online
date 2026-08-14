8d8d9a1482ed1678f40b72c92adf891ba510ecb1
claudioarg <claudioariascad@gmail.com>
2026-08-14 18:56:30 -0300

    chore: usar URL publica en render para red y logs

--- server/src/network.js
+++ server/src/network.js
@@ -1,6 +1,11 @@
 const os = require('os');
 
 function getLocalIP() {
+  const external = process.env.RENDER_EXTERNAL_URL || process.env.VERCEL_URL || process.env.FLY_APP_NAME;
+  if (external) {
+    return external.replace(/^https?:\/\//, '').replace(/\/$/, '');
+  }
+
   const interfaces = os.networkInterfaces();
   for (const name of Object.keys(interfaces)) {
     for (const iface of interfaces[name]) {
